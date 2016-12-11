6b92603afd6a1f24b71f7aba41d4ec18e2cee1db
Sebastian Rutofski <sebastian.rutofski@gmail.com>
2016-12-11 16:30:16 +0100

    0.2

--- woowee-improver.user.js
+++ woowee-improver.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name         Better WooWee
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       SebRut
-// @match        https://woowee.de/aufgaben/
+// @match        https://woowee.de/*
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
@@ -12,20 +11,21 @@
 
 var timeRegEx = /(\d+) Min. (\d+) Sek./;
 var secsTillNextTask = 0;
+var autoAcceptAvTasks = false;
 
 function updateTaskTimer() {
     secsTillNextTask--;
-    if(secsTillNextTask < 0) {
+    if(secsTillNextTask <= 0) {
         location.reload();
         return;
     }
-    taskTimer.text((secsTillNextTask > 60 ? Math.floor(secsTillNextTask / 60) + " Minute(s), " : "") + secsTillNextTask%60 + " Second(s)");
+    taskTimer.text((secsTillNextTask >= 60 ? Math.floor(secsTillNextTask / 60) + " Minute(s), " : "") + secsTillNextTask%60 + " Second(s)");
 
     setTimeout(updateTaskTimer, 1000);
 }
 
 function prepareTaskTimer() {
-    var nextTaskEl = $("body > div.body > div > div:nth-child(4) > div > div.col-md-9 > div:nth-child(7) > table > tbody > tr").first();
+    var nextTaskEl = $("body > div.body > div > div:nth-child(4) tbody").last().find("tr").first();
 
     if(nextTaskEl !== null) {
         var timeEl = $(nextTaskEl).find("td:nth-child(4)");
@@ -33,7 +33,7 @@ function prepareTaskTimer() {
             var mins = timeEl.text().match(timeRegEx)[1];
             var secs = timeEl.text().match(timeRegEx)[2];
             secsTillNextTask = eval(mins * 60) + eval(secs);
-            console.log(mins + " " + secs + " " + secsTillNextTask);
+            console.log("Next task in " + mins + " mins, " + secs + " secs, = " + secsTillNextTask + " total secs");
             setTimeout(updateTaskTimer, 1000);
             return;
         }
@@ -42,10 +42,11 @@ function prepareTaskTimer() {
 }
 
 var taskTimer;
+var autoAcceptToggleButton;
 
 function setUpUI() {
-    $("body > div.body > div > div:nth-child(4) > div > div.col-md-9 > p").hide();
-    
+    //$("body > div.body > div > div:nth-child(4) > div > div.col-md-9 > p.aligncenter.mt-100.mb-100").hide();
+    $("body > div.body > div > div:nth-child(4) > div > div.col-md-9 > p:nth-child(5)").hide();
     var dashboardRoot = $("body > div.body > div > div:nth-child(2)");
     dashboardRoot.empty();
 
@@ -68,7 +69,7 @@ function setUpUI() {
     taskTimerCell.append(taskTimer);
     row.append(taskTimerCell);
     tBody.append(row);
-    
+
     row = $(document.createElement('tr'));
     var leftCell = $(document.createElement('td'));
     leftCell.text("Revenue/Payout Limit");
@@ -77,13 +78,33 @@ function setUpUI() {
     rightCell.addClass("alignright");
     var val = $(document.createElement('strong'));
     var rev = $("body > div.body > div > div:nth-child(4) > div > div.col-md-3 > div > aside > p > strong:nth-child(3)").text().replace(" €", "").replace(",",".");
-    var percentage = Math.floor(eval(rev)/5 * 100);
-    val.text(rev + "€ / " + percentage + "%");
+    var percentage = (eval(rev)/5 * 100).toFixed(2);
+    val.text(parseFloat(rev).toFixed(2) + "€ / " + percentage + "%");
     rightCell.append(val);
     row.append(rightCell);
-    
-    
     tBody.append(row);
+
+    row = $(document.createElement('tr'));
+    var leftCell = $(document.createElement('td'));
+    leftCell.text("Auto accept New Task");
+    row.append(leftCell);
+    rightCell = $(document.createElement('td'));
+    rightCell.addClass("alignright");
+    autoAcceptToggleButton = $(document.createElement('button'));
+    autoAcceptToggleButton.addClass("btn btn-primary");
+    autoAcceptToggleButton.attr("type", "button");
+    autoAcceptToggleButton.attr("data-toggle", "button");
+    autoAcceptToggleButton.attr("aria-pressed", autoAcceptAvTasks);
+    autoAcceptToggleButton.attr("autocomplete", "off");
+    autoAcceptToggleButton.text(autoAcceptAvTasks ? "On" : "Off");
+    autoAcceptToggleButton.click(function() {
+        autoAcceptAvTasks = !eval(autoAcceptToggleButton.attr("aria-pressed"));
+        autoAcceptToggleButton.text(autoAcceptAvTasks ? "On" : "Off");
+    });
+    rightCell.append(autoAcceptToggleButton);
+    row.append(rightCell);
+    tBody.append(row);
+
     table.append(tBody);
     tDiv.append(table);
     dashboardRoot.append(tDiv);
@@ -91,10 +112,66 @@ function setUpUI() {
     prepareTaskTimer();
 }
 
+function loadSettings() {
+    autoAcceptAvTasks = eval(localStorage.autoAcceptAvTasks);
+    if(autoAcceptAvTasks === undefined) autoAcceptAvTasks = false;
+    console.log("settings loaded");
+}
+
+function saveSettings() {
+    localStorage.autoAcceptAvTasks = autoAcceptAvTasks;
+    console.log("settings saved");
+}
+
+function taskPage() {
+    setUpUI();
+    if(autoAcceptAvTasks) {
+        var tbodies = $("body > div.body > div > div:nth-child(4) tbody").not("#task_run");
+        if (tbodies.length >= 2) {
+            $(tbodies.first()).find("tr").first().click();
+        }
+
+        setTimeout(function() {
+            if($("#modal_form > div > strong").first().text().indexOf("Hoppla") != -1) {
+                location.reload();
+            }
+            else if($("#submit").first().text().indexOf("abbrechen") == -1) {
+                $("#submit").click();
+            }}, 1000);
+    }
+}
+
+function ajaxPage() {
+        setTimeout(function() {
+            if($("#modal_form > div > strong").first().text().indexOf("Hoppla") != -1) {
+                location.reload();
+            }
+            else if($("#submit").first().text().indexOf("abbrechen") == -1) {
+                $("#submit").click();
+                
+                setTimeout(function() { location.pathname="/aufgaben"; }, 1000);
+            }}, 1000);
+}
+
 (function() {
     'use strict';
 
-    setUpUI();
-    $("body > div.body > div > div:nth-child(4) > div > div.col-md-9 > div:nth-child(3) > table > tbody > tr:nth-child(1)").trigger("click");
-setTimeout(function() {$("#submit").trigger("click");}, 500);
+    window.onbeforeunload = saveSettings;
+    loadSettings();
+
+    if(location.pathname.indexOf("/ajax") != -1) {
+ajaxPage();
+    } 
+    if(location.pathname.indexOf("/login") != -1) {
+        location.pathname = "/aufgaben";
+    }
+    if(location.search !== "") {
+        location.search = "";
+    }
+
+    if(location.pathname.indexOf("/aufgaben/") != -1) {
+        taskPage();
+    }
+
+
 })();
